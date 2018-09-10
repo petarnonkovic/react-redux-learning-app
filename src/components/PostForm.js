5014a118750026e5e1de2d661078d64885cba136
unknown <tekumseh777@gmail.com>
2018-09-11 01:03:02 +0200

    Refactored to use Redux

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createPost } from '../actions/postActions';
+
 
 class PostForm extends Component {
 
@@ -20,15 +24,9 @@ class PostForm extends Component {
             title: this.state.title,
             body: this.state.body
         };
-        fetch('https://jsonplaceholder.typicode.com/posts', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(post)
-        })
-        .then(res => res.json())
-        .then(data => console.log(data));
+
+        this.props.createPost(post);
+
     }
 
     render() {
@@ -55,4 +53,10 @@ class PostForm extends Component {
     }
 }
 
-export default PostForm;
+PostForm.propTypes = {
+    createPost: PropTypes.func.isRequired
+}
+
+
+
+export default connect(null, { createPost })(PostForm);
